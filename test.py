ded2367d0e57de36fb69a8dc893029f79f7ddc5f
Alexander Yasinski <avyasinski@gmail.com>
2021-03-09 15:47:38 +0300

    added new code

--- test.py
+++ test.py
@@ -1,17 +1,34 @@
-# объявление функции
-def is_palindrome(text):
-    #text = ''.join(text.strip('.').strip('!').strip('?').lower().split())
-    new = ''
-    for i in text:
-        if i.isalpha() == True:
-            new += i.lower()
-    print(new)
-    if new == new[::-1]:
+def is_prime(num):
+    flag = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            flag = False
+            break
+    if num > 1 and flag == True:
         return True
     else:
         return False
+
+# объявление функции
+def is_valid_password(password):
+    p = password.split(':')
+    if len(p) > 3:
+        return False
+    f1, f2, f3 = False, False, False
+    if p[0] == p[0][::-1]:
+        f1 = True
+    if is_prime(int(p[1])):
+        f2 = True 
+    if int(p[2]) % 2 == 0:
+        f3 = True
+    print(f1, f2, f3)
+    if f1 == f2 == f3 == True:
+        return True
+    else:
+        return False
+
 # считываем данные
-txt = 'Gabler Ruby - burrel bag!'
+psw = '1221:101:22:22'
 
 # вызываем функцию
-print(is_palindrome(txt))
+print(is_valid_password(psw))
